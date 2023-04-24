44057c228546324b784c1e79abc0d73e2886cdbe
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-04-24 11:09:01 -0300

    correção para deploy

--- src/app.js
+++ src/app.js
@@ -112,6 +112,8 @@ app.get("/records", async (req, res) => {
     }
 })
 
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
+const PORT = process.env.PORT || 5000 
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
